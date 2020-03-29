93687fdf6f6e3ecdd10c7f8111b88344f66609be
daniboomerang <estevez.dani@gmail.com>
2020-03-29 13:25:03 +0700

    Install ant-design library.
    Improve pages UX.
    
    Install styled-components library.

---
+++ client/src/components/Notes/Note.js
@@ -0,0 +1,102 @@
+import React, { useRef, useState } from 'react';
+import useOnclickOutside from 'react-cool-onclickoutside';
+import styled from 'styled-components';
+import { CloseCircleTwoTone } from '@ant-design/icons';
+import { Affix, Input, message } from 'antd';
+import { blue } from '@ant-design/colors';
+
+const MAX_CHARACTERS_PER_LINE = 77;
+
+const NoteContent = styled.div`
+  grid-template-columns: 1fr auto;
+  justify-content: space-between;
+  grid-column-gap: 50px;
+  align-items: start;
+  display: grid;
+`;
+
+const Actions = styled.div.attrs(({ isHovered }) => ({
+  visibility: !isHovered ? 'hidden' : 'visible'
+}))`
+  visibility:  ${props => props.visibility};
+`;
+
+const NoteTxt = styled.div`
+  white-space: pre-line;
+`;
+
+const { TextArea } = Input;
+
+const Note = ({ id, note, onUpdateNote, onDeleteNote, parentScrollableContainer }) => {
+  const ref = useRef();
+
+  // State hooks
+  const [updateOngoing, setUpdateOngoing] = useState(false);
+  const [newNote, setNewNote] = useState(note);
+  const [isHovered, setIsHovered] = useState(false);
+
+  // Handlers
+  const handleUpdateOngoing = () => setUpdateOngoing(true);
+
+  const handleOnChangeNote = e => setNewNote(e.target.value);
+
+  const handleUpdateNote = (e) => {
+    if((!e && newNote !== '') || (e && newNote && e.key === 'Enter' && !e.shiftKey)) {
+      onUpdateNote({ variables: { _id: id, content: newNote } })
+        .catch(updateNoteMutationError => {
+          message.error(updateNoteMutationError);
+        }).finally(() => {
+          setUpdateOngoing(false);
+        });
+    }
+  };
+
+  const handleDeleteNote = (e) => {
+    e.stopPropagation();
+
+    onDeleteNote({ variables: { _id: id }, refetchQueries: ['NotesQuery'] }).then(() => {
+      message.success('Note deleted');
+    }).catch(deleteNoteMutationError => {
+      message.error(deleteNoteMutationError);
+    });
+  };
+
+  const handleOnMouseOver = () => setIsHovered(true);
+  const handleOnMouseLeave = () => setIsHovered(false);
+
+  useOnclickOutside(ref, () => {
+    if(updateOngoing) {
+      handleUpdateNote();
+    }
+  });
+
+  return (
+    // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+    <NoteContent
+      onClick={handleUpdateOngoing}
+      onMouseOver={handleOnMouseOver}
+      onMouseLeave={handleOnMouseLeave}
+    >
+      <div ref={ref}>
+        {updateOngoing ? (
+          <TextArea
+            rows={Math.round(newNote.length / MAX_CHARACTERS_PER_LINE) + 1 | 1 }
+            value={newNote}
+            onChange={handleOnChangeNote}
+            onKeyDown={handleUpdateNote}
+          />
+
+        ) : (
+          <NoteTxt>{ newNote }</NoteTxt>
+        )}
+      </div>
+      <Affix target={() => parentScrollableContainer} offsetTop={25}>
+        <Actions isHovered={isHovered}>
+          <CloseCircleTwoTone twoToneColor={blue.primary} onClick={handleDeleteNote} style={{ fontSize: '20px' }} />
+        </Actions>
+      </Affix>
+    </NoteContent>
+  );
+};
+
+export default Note;
--- client/src/screens/notes/NotesPage.js
+++ client/src/screens/notes/NotesPage.js
@@ -1,7 +1,50 @@
 import React, { useState } from 'react';
 import { useMutation, useQuery } from 'react-apollo';
+import styled, { css } from 'styled-components';
 import { CreateNoteMutation, DeleteNoteMutation, UpdateNoteMutation }  from './graphql/mutations';
 import NotesQuery from './graphql/queries';
+import Page from '../../components/Layout/Page';
+import Note from '../../components/Notes/Note';
+import { Button, Input, message } from 'antd';
+
+const NotesContent = styled.div`
+  box-shadow: 0px 0px 3px 1px #f5f5f5;
+  border-radius: 10px;
+  max-width: 600px;
+  overflow: hidden;
+`;
+
+const pageSectionStyle = css`
+  background: white;
+`;
+
+const CreateNoteSection = styled.div`
+  border-bottom: solid 1px #f5f5f5;
+  padding: 20px;
+  display: flex;
+  ${pageSectionStyle}
+`;
+
+const NotesListSection = styled.div`
+  overflow-y: scroll;
+  height: 510px;
+  ${pageSectionStyle}
+`;
+
+const ListElement = styled.div`
+  border-bottom: solid 1px #f5f5f5;
+  padding:  15px 0px 15px 10px;
+  padding: 20px;
+
+  &:hover {
+    background: #fafafa;
+    cursor: pointer;
+  }
+    
+  &:last-child {
+    border-bottom: none;
+  }
+`;
 
 const NotesPage = () => {
   // Graphql query
@@ -9,97 +52,63 @@ const NotesPage = () => {
 
   // State hooks
   const [newNote, setNewNote] = useState('');
-  const [noteContentBeingUpdated, setNoteContentBeingUpdated] = useState('');
-  const [noteIdBeingUpdated, setNoteIdBeingUpdated] = useState('');
+  const [listScrollableContainer, setListScrollableContainer] = useState(null);
 
   // Graphql mutations
   const [createNoteMutation] = useMutation(CreateNoteMutation);
   const [deleteNoteMutation] = useMutation(DeleteNoteMutation);
   const [updateNoteMutation] = useMutation(UpdateNoteMutation);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  const { notes } = data;
+  if (error) {
+    message.error('Error :(');
+    return null;
+  }
 
   // Handlers
   const handleCreateNote = () => {
     if (newNote) {
       createNoteMutation({ variables: { content: newNote }, refetchQueries: ['NotesQuery'] }).then(() => {
-        // eslint-disable-next-line no-console
-        console.log('Response received from server.');
+        message.success('Note created');
       }).catch(createNoteMutationError => {
-        // eslint-disable-next-line no-console
-        console.log(createNoteMutationError);
+        message.error(createNoteMutationError);
       });
       setNewNote('');
     }
   };
-  const handleUpdateNote = (_id, content, isBeingUpdated) => () => {
-    if (isBeingUpdated) {
-      updateNoteMutation({ variables: { _id, content: noteContentBeingUpdated } }).then(() => {
-        // eslint-disable-next-line no-console
-        console.log('Response received from server.');
-      }).catch(updateNoteMutationError => {
-        // eslint-disable-next-line no-console
-        console.log(updateNoteMutationError);
-      });
-      setNoteIdBeingUpdated('');
-      setNoteContentBeingUpdated('');
-    } else {
-      setNoteIdBeingUpdated(_id);
-      setNoteContentBeingUpdated(content);
-    }
-  };
-  const handleNoteBeingUpdated = (e) => setNoteContentBeingUpdated(e.target.value);
-  const handleDeleteNote = (_id) => () => {
-    deleteNoteMutation({ variables: { _id }, refetchQueries: ['NotesQuery'] }).then(() => {
-      // eslint-disable-next-line no-console
-      console.log('Response received from server.');
-    }).catch(deleteNoteMutationError => {
-      // eslint-disable-next-line no-console
-      console.log(deleteNoteMutationError);
-    });
-  };
-  const handleOnChangeNote = e => setNewNote(e.target.value);
+
+  const handleOnChangeNewNote = e => setNewNote(e.target.value);
 
   return (
-    <div>
-      <header>Notes</header>
-      <ul>
-        {notes.map(note => {
-          const { content, _id } = note;
-          const isBeingUpdated = noteIdBeingUpdated === _id;
+    <Page currentPage="notes" loading={loading}>
+      <NotesContent>
+        <CreateNoteSection>
+          <Input
+            value={newNote}
+            onChange={handleOnChangeNewNote}
+            placeholder="Add a note here"
+            style={{  height: '50px' }}
+          />
+          <Button type="primary" onClick={handleCreateNote} style={{  height: '50px' }}>Create note</Button>
+        </CreateNoteSection>
+        <NotesListSection ref={setListScrollableContainer}>
+          {data?.notes.map(note => {
+            const { content, _id } = note;
 
-          return (
-            <div key={_id}>
-              {isBeingUpdated ? (
-                <li>
-                  <input
-                    value={noteContentBeingUpdated}
-                    onChange={handleNoteBeingUpdated}
-                  />
-                </li>
-              ) : (
-                <li>{content}</li>
-              )}
-              <div style={{ display: 'flex' }}>
-                <button type="button" onClick={handleUpdateNote(_id, content, isBeingUpdated)}>update</button>
-                <button type="button" onClick={handleDeleteNote(_id)}>delete</button>
-              </div>
-            </div>
-          );
-        })}
-      </ul>
-      <footer>
-        <input
-          value={newNote}
-          onChange={handleOnChangeNote}
-          placeholder='Add a note here'
-        />
-        <button type="button" onClick={handleCreateNote}>create note</button>
-      </footer>
-    </div>
+            return (
+              <ListElement key={_id}>
+                <Note
+                  id={_id}
+                  note={content}
+                  onUpdateNote={updateNoteMutation}
+                  onDeleteNote={deleteNoteMutation}
+                  parentScrollableContainer={listScrollableContainer}
+                />
+              </ListElement>
+            );
+          })}
+        </NotesListSection>
+      </NotesContent>
+    </Page>
   );
 };
 
